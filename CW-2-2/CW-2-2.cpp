347669e02d8a82737a6caf3faa76a33a4bad4559
XamaX <Kriuchkov.Dmitry@at.urfu.ru>
2021-10-02 19:13:17 +0500

    [CW-2-2] Практика 2. Неделя 2. (Исправление ошибок)

--- CW-2-2/CW-2-2.cpp
+++ CW-2-2/CW-2-2.cpp
@@ -7,27 +7,22 @@
 int main() {
     std::string wordIn;
     std::cin >> wordIn;
-    int firstCounter = 0, secondCounter = 0;
+    int counter = 0;
 
     for (int i = 0; i < wordIn.size(); i++) {
         if (wordIn[i] == 'f') {
-            firstCounter++;
+            counter++;
+            if (counter == 2) {
+                double index = i;
+                std::cout << index;
+            }
         }
     }
 
-    if (firstCounter == 0) {
+    if (counter == 0) {
         std::cout << -2;
-    } else if (firstCounter == 1) {
+    } else if (counter == 1) {
         std::cout << -1;
-    } else {
-        for (int i = 0; i < wordIn.size(); i++) {
-            if (wordIn[i] == 'f') {
-                secondCounter++;
-                if (secondCounter == 2) {
-                    std::cout << i;
-                }
-            }
-        }
     }
 
     return 0;
