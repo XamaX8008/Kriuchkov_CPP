3ea2d2ee48337d0fb4ba7cc7cd4572392109b7fe
XamaX <Kriuchkov.Dmitry@at.urfu.ru>
2021-10-02 18:22:19 +0500

    [CW-2-2] Практика 2. Неделя 2. (Исправление ошибок)

--- HW-1-1/HW-1-1.cpp
+++ HW-1-1/HW-1-1.cpp
@@ -4,34 +4,34 @@
 
 #include <iostream>
 #include <cmath>
-using namespace std;
-int main() {
-    double x1, x2, x, D, a, b, c;
 
+using namespace std;
+
+int main() {
+    double a, b, c;
     cin >> a >> b >> c;
-    if (a != 0 && b != 0 && c != 0) {
-        D = b * b - 4 * a * c;
-        if (D < 0) {
+
+    if ((a != 0 && b != 0) || (a != 0 && b == 0)) {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
             return 0;
-        } else if (D == 0) {
-            x = -b / (2 * a);
-            cout << x;
+        } else if (discriminant == 0) {
+            double firstX = -b / (2 * a);
+            cout << firstX;
         } else {
-            x1 = (-b + sqrt(D)) / (2 * a);
-            x2 = (-b - sqrt(D)) / (2 * a);
-            cout << x1 << " " << x2;
+            double firstX = (-b + sqrt(discriminant)) / (2 * a);
+            double secondX = (-b - sqrt(discriminant)) / (2 * a);
+            cout << firstX << " " << secondX;
         }
-    } else if ((b == 0 && c == 0) || (a == 0 && b == 0)) {
-        cout << 0;
+    } else if (a == 0 && b == 0) {
+        cout << "Coefficients entered incorrectly";
     } else if (a == 0) {
-        x = (-c) / b;
-        cout << x;
+        double firstX = (-c) / b;
+        cout << firstX;
     } else if (b == 0) {
-        x = sqrt((-c) / b);
-        cout << x;
-    } else if(c == 0) {
-        x1 = 0;
-        x2 = (-b) / a;
-        cout << x1 << " " << x2;
+        double firstX = sqrt((-c) / b);
+        cout << firstX;
     }
+
+    return 0;
 }
