fb0927c1a78871314c5f05b425b301cb4d267da2
XamaX <Kriuchkov.Dmitry@at.urfu.ru>
2021-10-02 18:50:33 +0500

    [CW-1-1] Практика 1. Неделя 1. (Исправление ошибок)

--- CW-1-1/CW-1-1.cpp
+++ CW-1-1/CW-1-1.cpp
@@ -1,32 +1,32 @@
 //
 // Created by XamaX on 25.09.2021.
 //
-
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
 int main()
 {
-    int x1, x2, x3, y1, y2, y3;
-    double a, b, c, p, r, q1, q2, relationship, x4, y4;
+    int firstCoordinateX, secondCoordinateX, thirdCoordinateX, firstCoordinateY, secondCoordinateY, thirdCoordinateY;
+    cin >> firstCoordinateX >> firstCoordinateY >> secondCoordinateX >> secondCoordinateY >> thirdCoordinateX
+        >> thirdCoordinateY;
 
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    double sideA = sqrt(abs((secondCoordinateX - firstCoordinateX) * (secondCoordinateX - firstCoordinateX) +
+            (secondCoordinateY - firstCoordinateY) * (secondCoordinateY - firstCoordinateY)));
+    double sideB = sqrt(abs((thirdCoordinateX - secondCoordinateX) * (thirdCoordinateX - secondCoordinateX) +
+            (thirdCoordinateY - secondCoordinateY) * (thirdCoordinateY - secondCoordinateY)));
+    double sideC = sqrt(abs((thirdCoordinateX - firstCoordinateX) * (thirdCoordinateX - firstCoordinateX) +
+            (thirdCoordinateY - firstCoordinateY) * (thirdCoordinateY - firstCoordinateY)));
+    double halfMeter = (sideA + sideB + sideC) / 2;
+    double distance = 2 / sideA * sqrt(halfMeter * (halfMeter - sideA) * (halfMeter - sideB) * (halfMeter - sideC));
+    double sideDivisionA1 = sqrt(sideC * sideC - distance * distance);
+    double sideDivisionA2 = sqrt(sideB * sideB - distance * distance);
+    double relationship = sideDivisionA1 / sideDivisionA2;
+    double fourthCoordinateX = (firstCoordinateX + relationship * secondCoordinateX) /  (1 + relationship);
+    double fourthCoordinateY = (firstCoordinateY + relationship * secondCoordinateY) /  (1 + relationship);
 
-    a = sqrt(abs((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-    b = sqrt(abs((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2)));
-    c = sqrt(abs((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1)));
+    cout << distance << " " << fourthCoordinateX << " " << fourthCoordinateY;
 
-    p = (a + b + c) / 2;
-
-    r = 2 / a * sqrt(p * (p - a) * (p - b) * (p - c));
-
-
-
-    q1 = sqrt(c * c - r *r );
-    q2 = sqrt(b * b - r * r);
-    relationship = q1 / q2;
-    x4 = (x1 + relationship * x2) /  (1 + relationship);
-    y4 = (y1 + relationship * y2) /  (1 + relationship);
-    cout << r << " " << x4 << " " << y4;
-
+    return 0;
 }
