74b2d02410bc301265d53de4a1883e83801376d3
XamaX <Kriuchkov.Dmitry@at.urfu.ru>
2021-10-10 16:49:46 +0500

    [HW-2-1] Домашнее задание 1. Неделя 2. (Исправление ошибок)

--- HW-2-1/HW-2-1.cpp
+++ HW-2-1/HW-2-1.cpp
@@ -7,13 +7,15 @@
 #include <cmath>;
 
 int main() {
-    int number, numberSystem;
-    std::cin >> number >> numberSystem;
+    int number, baseOfNumberSystem;
+    std::cin >> number >> baseOfNumberSystem;
 
-    int result = 0, count = 0;
+    int result = 0;
+    int count = 0;
+
     while (number != 0) {
-        result += (number % numberSystem) * pow(10, count);
-        number /= numberSystem;
+        result += (number % baseOfNumberSystem) * pow(10, count);
+        number /= baseOfNumberSystem;
         count++;
     }
 
