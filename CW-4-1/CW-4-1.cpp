98079ac8d21f98ce7b611909a2b24b90f8818264
XamaX <Kriuchkov.Dmitry@at.urfu.ru>
2021-10-21 21:22:22 +0500

    [HW-3-2] Домашнее задание 2. Неделя 3. (Исправление ошибок 3)

--- CW-4-1/CW-4-1.cpp
+++ CW-4-1/CW-4-1.cpp
@@ -8,32 +8,33 @@
 
 using namespace std;
 
-vector<string> finder(int min, vector<string> strings) {
-    vector<string> result;
+vector <string> palindromFilter(int min, vector <string> strings) {
+    vector <string> result;
 
     for (int i = 0; i < strings.size(); i++) {
-        bool bflag = true;
+        bool flag = true;
         string string = strings[i];
-        for (int j = 0; j < string.length(); j++) {
-            if (string[j] != string[string.length() - j - 1]) {
-                bflag = false;
+        if (string.length() >= min) {
+            for (int j = 0; j < string.length(); j++) {
+                if (string[j] != string[string.length() - j - 1]) {
+                    flag = false;
+                }
             }
-        }
-        if (bflag && string.length() >= min) {
-            result.push_back(string);
+            if (flag) {
+                result.push_back(string);
+            }
         }
     }
 
     return result;
 }
 
-int main()
-{
-    vector<string> words;
+int main() {
+    vector <string> words;
     words.push_back("abacaba");
     words.push_back("aba");
     words.push_back("code");
-    vector<string> result = finder(4, words);
+    vector <string> result = palindromFilter(4, words);
 
     for (int k = 0; k < result.size(); k++) {
         cout << result[k] << " ";
